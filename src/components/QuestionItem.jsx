f13f2349bfbf5fbf3a83b9b008d9f79cdffe0c3b
ittuann <ittuann@outlook.com>
2025-03-28 09:17:39 +0800

    update question item size

--- src/components/QuestionItem.jsx
+++ src/components/QuestionItem.jsx
@@ -10,13 +10,13 @@ class QuestionItem extends Component {
           {question.id}. {question.text}
         </p>
         <div className="flex items-center justify-center my-6">
-          <div className="text-lg text-gray-700 items-center text-center break-words mr-6">
+          <div className="text-gray-700 items-center text-center break-words mr-2 md:mr-6">
             {degree[0]}
           </div>
-          <div className="flex gap-4 md:gap-8 items-center">
+          <div className="flex gap-1 md:gap-8 items-center">
             {question.scoresAagree.map((val, index) => {
               // 圆圈尺寸
-              let size = "w-4 h-4 md:w-6 md:h-6";
+              let size = "w-5 h-5 md:w-6 md:h-6";
               if (index === 0 || index === question.scoresAagree.length - 1) {
                 // 两端最大
                 size = "w-8 h-8 md:w-9 md:h-9";
@@ -25,7 +25,7 @@ class QuestionItem extends Component {
                 index === question.scoresAagree.length - 2
               ) {
                 size = "w-7 h-7 md:w-8 md:h-8";
-              } else if (index === 2) {
+              } else if (index === question.scoresAagree.length - 3) {
                 size = "w-6 h-6 md:w-7 md:h-7";
               }
 
@@ -48,7 +48,7 @@ class QuestionItem extends Component {
               );
             })}
           </div>
-          <div className="text-lg text-gray-700 items-center text-center break-words ml-6">
+          <div className="text-gray-700 items-center text-center break-words ml-2 md:ml-6">
             {degree[1]}
           </div>
         </div>
