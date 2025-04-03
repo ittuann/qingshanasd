5b93442e55b20a693a33433488b82105b2dd6e7f
ittuann <ittuann@outlook.com>
2025-04-03 19:47:16 +0800

    update question result tip

--- src/components/QuestionInfo.jsx
+++ src/components/QuestionInfo.jsx
@@ -3,16 +3,14 @@ import "remixicon/fonts/remixicon.css";
 
 const QuestionInfo = ({ icon, iconBg = "bg-rose-100", content }) => {
   return (
-    <div className="bg-gray-50 border border-gray-100 rounded-md p-4 mt-3">
-      <div className="flex justify-center items-center">
-        <div className="inline-flex items-center">
-          <div
-            className={`flex-shrink-0 w-8 h-8 ${iconBg} rounded-full flex items-center justify-center`}
-          >
-            {icon}
-          </div>
-          <div className="ml-3 text-left">{content}</div>
-        </div>
+    <div className="bg-gray-50 border border-gray-100 rounded-lg p-4 mt-3">
+      <div className="inline-flex items-center justify-center">
+        <span
+          className={`flex-shrink-0 w-8 h-8 ${iconBg} rounded-full flex items-center justify-center`}
+        >
+          {icon}
+        </span>
+        <span className="ml-3 text-left">{content}</span>
       </div>
     </div>
   );
